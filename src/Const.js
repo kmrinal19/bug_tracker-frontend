78e9a2eb2ec1a14fc849ca58a4e5657fd89a666f
Mrinal <kmrinal19@gmail.com>
2020-05-24 00:05:04 +0530

    wrote some boilerplate code

---
+++ src/Const.js
@@ -0,0 +1,2 @@
+export const CLIENT_ID = '0ODR6pyyZcggO7YYkFHGssujPTaRPAKSuZCiCCln'
+export const REDIRECT_URL = 'http://localhost:3000/'
---
+++ src/components/login.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Header, Image, Container, Label } from 'semantic-ui-react'
+import { CLIENT_ID, REDIRECT_URL } from '../Const'
+
+const Login = () => {
+    return (
+        <div>
+            <Image
+                centered
+                size = 'small'
+                src = {require('../images/logo.png')}
+            />
+            <Header as = 'h2' textAlign = 'center'>
+                <Header.Content>
+                    Bug Tracker
+                </Header.Content>
+            </Header>
+            <Container text>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                    ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
+                    magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+                    ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+                    quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+                    arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+                    Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
+                    dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+                    Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
+                    Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+                    viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
+                    Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
+                </p>
+                <br/>
+                <Label as = 'a'
+                    basic
+                    size = 'large'
+                    href = 'http://localhost:3000/home/'
+                    >
+                    <Image spaced = 'right' src = {require('../images/omniport.ico')}/>
+                    Login with omni:port
+                </Label>
+            </Container>
+        </div>
+    )
+}
+ export default Login
